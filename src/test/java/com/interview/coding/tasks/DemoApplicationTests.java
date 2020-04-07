63cabe22c07ff5fe830a1bb8871f19cfaed64c71
Aliaksei Tatarynchyk <aliaksei_tatarynchyk@epam.com>
2020-04-07 15:58:39 -0700

    Added stub for SpringBoot+gradle application

---
+++ src/test/java/com/interview/coding/tasks/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.coding.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
