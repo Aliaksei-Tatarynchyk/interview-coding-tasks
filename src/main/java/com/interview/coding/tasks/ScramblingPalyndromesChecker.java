a6a036e859bd669485383131cb6b755dbf411707
Aliaksei Tatarynchyk <aliaksei_tatarynchyk@epam.com>
2020-04-08 15:59:54 -0700

    Simplified ScramblingPalyndromesChecker, return type was changed to boolean, used streams and some other Java 8 api.

--- src/main/java/com/interview/coding/tasks/ScramblingPalyndromesChecker.java
+++ src/main/java/com/interview/coding/tasks/ScramblingPalyndromesChecker.java
@@ -3,6 +3,8 @@ package com.interview.coding.tasks;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Checks whether 2 words are palyndromes. Palyndromes in this particular context means
@@ -23,81 +25,55 @@ public class ScramblingPalyndromesChecker {
      *
      * @param firstWord
      * @param secondWord
-     * @return result of checking palyndromes.
+     * @return true if words are palyndromes.
      */
-    public Result checkWhetherWordsArePalyndromes(String firstWord, String secondWord) {
+    public boolean checkWhetherWordsArePalyndromes(String firstWord, String secondWord) {
         if (firstWord == null && secondWord == null) {
-            return new Result(firstWord, secondWord, true, "both words are null");
+            return true;
         }
 
         if (firstWord == null || secondWord == null) {
-            return new Result(firstWord, secondWord, false, "one of words is null and another is not null");
+            return false;
         }
 
         if (firstWord.equals(secondWord)) {
-            return new Result(firstWord, secondWord, true, "words are equal");
+            return true;
         }
 
         if (firstWord.length() != secondWord.length()) {
-            return new Result(firstWord, secondWord, false, "words have different lengths");
+            return false;
         }
 
         Map<Character, Integer> wordCharOccurrences = new HashMap<>(firstWord.length());
 
-        for (int i = 0; i < firstWord.length(); i++) {
-            increaseOccurence(wordCharOccurrences, firstWord.charAt(i));
-            decreaseOccurence(wordCharOccurrences, secondWord.charAt(i));
-        }
+        // I'm not sure it's better than the option below, it is neither shorter nor clearer. But it uses Java 8.
+        IntStream.range(0, firstWord.length()).forEach(i -> {
+            wordCharOccurrences.merge(firstWord.charAt(i), 1, (current, update) -> current + update);
+            wordCharOccurrences.merge(secondWord.charAt(i), -1, (current, update) -> current + update);
+        });
+
+//        for (int i = 0; i < firstWord.length(); i++) {
+//            wordCharOccurrences.merge(firstWord.charAt(i), 1, (current, update) -> current + update);
+//            wordCharOccurrences.merge(secondWord.charAt(i), -1, (current, update) -> current + update);
+//        }
+
+//        for (int i = 0; i < firstWord.length(); i++) {
+//            wordCharOccurrences.put(firstWord.charAt(i), wordCharOccurrences.getOrDefault(character, 0) + 1);
+//            wordCharOccurrences.put(secondWord.charAt(i), wordCharOccurrences.getOrDefault(character, 0) - 1);
+//        }
+
+//        for (int i = 0; i < firstWord.length(); i++) {
+//            increaseOccurence(wordCharOccurrences, firstWord.charAt(i));
+//            decreaseOccurence(wordCharOccurrences, secondWord.charAt(i));
+//        }
 
         for (Entry<Character, Integer> wordCharOccurrence : wordCharOccurrences.entrySet()) {
             if (wordCharOccurrence.getValue() != 0) {
-                return new Result(firstWord, secondWord, false, "words have different sets of chars");
+                return false;
             }
         }
 
-        return new Result(firstWord, secondWord, true, "words are palyndromes");
+        return true;
     }
 
-    private Integer increaseOccurence(Map<Character, Integer> map, char character) {
-        return map.put(character, map.getOrDefault(character, 0) + 1);
-    }
-
-    private Integer decreaseOccurence(Map<Character, Integer> map, char character) {
-        return map.put(character, map.getOrDefault(character, 0) - 1);
-    }
-
-    public static class Result {
-        String firstWord;
-        String secondWord;
-
-        /** True if words are scrambling palyndromes - one word can be made from another by changing order of characters */
-        boolean success;
-
-        /** Explanation of why the words are scrambling palyndromes */
-        String explanation;
-
-        public Result(String firstWord, String secondWord, boolean success, String explanation) {
-            super();
-            this.firstWord = firstWord;
-            this.secondWord = secondWord;
-            this.success = success;
-            this.explanation = explanation;
-        }
-
-        public String getFirstWord() {
-            return firstWord;
-        }
-
-        public String getSecondWord() {
-            return secondWord;
-        }
-
-        public boolean isSuccess() {
-            return success;
-        }
-
-        public String getExplanation() {
-            return explanation;
-        }
-    }
 }
